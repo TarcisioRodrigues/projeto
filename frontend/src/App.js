2e4b336547f2a5d895e69baf28e652bd1ec69c7e
Tarcisio Rodrigues <tarcisiorodrigues454@gmail.com>
2022-06-22 22:35:15 -0300

    initial

---
+++ frontend/src/App.js
@@ -0,0 +1,9 @@
+
+
+export const  App=()=> {
+  return (
+    <h1>dadawdawd</h1>
+  );
+}
+
+export default App;
---
+++ frontend/src/Pages/Home/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+
+export const Home = () => {
+  return (
+    <div className="Container">
+      <h1>Teste</h1>
+    </div>
+  );
+}
+
