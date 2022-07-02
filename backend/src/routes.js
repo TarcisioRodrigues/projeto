9947fe31594227473f1c5c6245d6b683322e63e3
Tarcisio Rodrigues <tarcisiorodrigues454@gmail.com>
2022-07-02 00:13:34 -0300

    pegando arquivos e lendo excel

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -2,6 +2,8 @@ import { Router } from 'express';
 import db from '../src/database/connection';
 import { ensureAuthenticate } from '../src/middlewares/ensureAuthenticate';
 import fs from 'fs'
+import readLine from 'readline'
+import {Readable} from 'stream'
 import multer from 'multer';
 import multerConfig from './config/multer';
 import pdfPrinter from 'pdfmake'
@@ -71,5 +73,35 @@ routes.get('/product/reports',async(request,response)=>{
   })
   
 })
-
+//Lendo Arquivo
+routes.post('product',upload.single(),(request,response)=>{
+  const {file }=request
+  const { buffer } = file;
+  
+  const readableFile = new Readable();
+  readableFile.push(buffer);
+  readableFile.push(null);
+  const productsLine = readline.createInterface({
+    input: readableFile,
+  })
+    const products=[]
+    //lendo os campos
+    for await (let line of productsLine) {
+     const productLineSplit=line.split(',')
+      products.push({
+        code_bar: productLineSplit[0],
+        description: productLineSplit[1],
+        price: Number(productLineSplit[2]),
+        quanity:Number( productLineSplit[3])
+      })
+      //inserindo os campos
+      for await (let {code_bar,description,price,quanity} of products){
+        await db('user').create({
+          code_bar,description,price,quanity
+        })
+      }
+    }
+  return response.send();
+  
+})
 export { routes };
